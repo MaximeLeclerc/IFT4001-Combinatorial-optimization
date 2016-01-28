6431d30960334ed24c6314de9485564288acf6a8
vincent.beaudoin <vincent.beaudoin@live.ca>
2016-01-28 18:06:25 -0500

    create file for first problem

---
+++ tp1/PremierProbleme.java
@@ -0,0 +1,12 @@
+import org.chocosolver.solver.constraints.*;
+import org.chocosolver.solver.search.strategy.*;
+import org.chocosolver.solver.variables.*;
+import org.chocosolver.solver.*;
+import org.chocosolver.solver.trace.*;
+import org.chocosolver.solver.search.limits.*;
+import org.chocosolver.solver.search.loop.monitors.*;
+
+public class PremierProbleme {
+    public static void main(String[] args) {
+    }
+}
