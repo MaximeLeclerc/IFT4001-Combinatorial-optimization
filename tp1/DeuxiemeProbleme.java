f7a05700ac3fc0598037f214c57f91a05d8a760b
vincent.beaudoin <vincent.beaudoin@live.ca>
2016-02-08 15:59:24 -0500

    add a constraint

--- tp1/DeuxiemeProbleme.java
+++ tp1/DeuxiemeProbleme.java
@@ -1,4 +1,5 @@
 import org.chocosolver.solver.constraints.*;
+import org.chocosolver.solver.constraints.extension.Tuples;
 import org.chocosolver.solver.search.strategy.*;
 import org.chocosolver.solver.variables.*;
 import org.chocosolver.solver.*;
@@ -9,6 +10,7 @@ import org.chocosolver.solver.search.loop.monitors.*;
 public class DeuxiemeProbleme {
 	public static final int NOMBRE_HEURES_MINIMUM = 5;
 	public static final int NOMBRE_HEURES_MAXIMUM = 7;
+	public static final int NOMBRE_EMPLOYES_MINIMUM = 1;
 
 	public static final int HEURISTIQUE_DEFAUT = 0;
 	public static final int HEURISTIQUE_DOMOVERWDEG = 1;
@@ -41,12 +43,26 @@ public class DeuxiemeProbleme {
 		else
 			lignes = VariableFactory.enumeratedMatrix("x", n, p, 0, 1, solver);
 
+		// Creation de la tranpose de la matrice lignes.
+		IntVar[][] colonnes = new IntVar[p][n];
+		for (int i = 0; i < p; i++) {
+			for (int j = 0; j < n; j++) {
+				colonnes[i][j] = lignes[j][i];
+			}
+		}
+
 		// Un employe doit travailler entre 5 et 7 heures.
 		IntVar variableNombreDemiHeuresMinimum = VariableFactory.fixed(NOMBRE_HEURES_MINIMUM * 2, solver);
 		IntVar variableNombreDemiHeuresMaximum = VariableFactory.fixed(NOMBRE_HEURES_MAXIMUM * 2, solver);
 		for (int i = 0; i < n; i++) {
 			solver.post(IntConstraintFactory.sum(lignes[i], ">=", variableNombreDemiHeuresMinimum));
 			solver.post(IntConstraintFactory.sum(lignes[i], "<=", variableNombreDemiHeuresMaximum));
+		}
+
+		// Chaque plage doit avoir un employe
+		IntVar variableNombreEmployesMinimum = VariableFactory.fixed(NOMBRE_EMPLOYES_MINIMUM, solver);
+		for (int i = 0; i < p; i++) {
+			solver.post(IntConstraintFactory.sum(colonnes[i], ">=", variableNombreEmployesMinimum));
 		}
 
 		// Vecteur contenant toutes les variables de la matrice dans un seul
@@ -99,5 +115,62 @@ public class DeuxiemeProbleme {
 			System.out.println("");
 		}
 		Chatterbox.printStatistics(solver);
+	}
+	
+	private static Tuples nombreEmployesSouhaite(int indice) {
+		int retour = 0;
+		
+		switch (indice) {
+		case 0:
+			retour = 1;
+			break;
+		case 1:
+			retour = 2;
+			break;
+		case 2:
+			retour = 3;
+			break;
+		case 3:
+			retour = 4;
+			break;
+		case 4:
+			retour = 5;
+			break;
+		case 5:
+			retour = 4;
+			break;
+		case 6:
+			retour = 2;
+			break;
+		case 7:
+			retour = 3;
+			break;
+		case 8:
+			retour = 4;
+			break;
+		case 9:
+			retour = 3;
+			break;
+		case 10:
+			retour = 5;
+			break;
+		case 11:
+			retour = 5;
+			break;
+		case 12:
+			retour = 4;
+			break;
+		case 13:
+			retour = 3;
+			break;
+		case 14:
+			retour = 3;
+			break;
+		case 15:
+			retour = 3;
+			break;
+		}
+
+		return retour;
 	}
 }
