29e14643fb98fc565741a9ab3b38debcf57c447c
Alexandre <alexandre.picardlemieux@gmail.com>
2016-04-12 11:41:40 -0400

    adding test for a bad frequencies and good

--- tp2/src/test/java/ift4001/tp2/VigenereSolverTest.java
+++ tp2/src/test/java/ift4001/tp2/VigenereSolverTest.java
@@ -12,12 +12,17 @@ public class VigenereSolverTest {
     private static final String CAESAR_CIPHER_TEXT = "YKBXG WLKHF TGLVH NGMKR FXGEX GWFXR HNKXT KLBVH FXMHU NKRVT XLTKG HMMHI KTBLX ABFMA XXOBE MATMF XGWHE BOXLT YMXKM AXFMA XZHHW BLHYM BGMXK KXWPB MAMAX BKUHG XLLHE XMBMU XPBMA VTXLT KMAXG HUEXU KNMNL ATMAM HEWRH NVTXL TKPTL TFUBM BHNLB YBMPX KXLHB MPTLT ZKBXO HNLYT NEMTG WZKBX OHNLE RATMA VTXLT KTGLP XKWBM AXKXN GWXKE XTOXH YUKNM NLTGW MAXKX LMYHK UKNMN LBLTG AHGHN KTUEX FTGLH TKXMA XRTEE TEEAH GHNKT UEXFX GVHFX BMHLI XTDBG VTXLT KLYNG XKTE";
     private static final String PLAIN_TEXT = "FRIEN DSROM ANSCO UNTRY MENLE NDMEY OUREA RSICO METOB URYCA ESARN OTTOP RAISE HIMTH EEVIL THATM ENDOL IVESA FTERT HEMTH EGOOD ISOFT INTER REDWI THTHE IRBON ESSOL ETITB EWITH CAESA RTHEN OBLEB RUTUS HATHT OLDYO UCAES ARWAS AMBIT IOUSI FITWE RESOI TWASA GRIEV OUSFA ULTAN DGRIE VOUSL YHATH CAESA RANSW ERDIT HEREU NDERL EAVEO FBRUT USAND THERE STFOR BRUTU SISAN HONOU RABLE MANSO ARETH EYALL ALLHO NOURA BLEME NCOME ITOSP EAKIN CAESA RSFUN ERAL";
     
-	private static final String PLAIN_TEXT_2 = "GENIUS WITHOUT EDUCATION IS LIKE SILVER IN THE MINE";
+	private static final String PLAIN_TEXT_SMALL = "GENIUS WITHOUT EDUCATION IS LIKE SILVER IN THE MINE";
+	
+	private static final String PLAIN_TEXT_GOOD_DIST_OF_LETTER = "HEREUPON LEGRAND AROSE WITH A GRAVE AND STATELY AIR AND BROUGHT ME THE BEETLE FROM A GLASS CASE IN WHICH IT WAS ENCLOSED IT WAS A BEAUTIFUL SCARABAEUS AND AT THAT TIME UNKNOWN TO NATURALISTS OF COURSE A GREAT PRIZE IN A SCIENTIFIC POINT OF VIEW THERE WERE TWO ROUND BLACK SPOTS NEAR ONE EXTREMITY OF THE BACK AND A LONG ONE NEAR THE OTHER THE SCALES WERE EXCEEDINGLY HARD AND GLOSSY WITH ALL THE APPEARANCE OF BURNISHED GOLD THE WEIGHT OF THE INSECT WAS VERY REMARKABLE AND TAKING ALL THINGS INTO CONSIDERATION I COULD HARDLY BLAME JUPITER FOR HIS OPINION RESPECTING IT";
+	private static final String PLAIN_TEXT_BAD_DIST_OF_LETTER = "ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB";
+	
     private static final String CAESAR_CIPHER_TEXT_2 = "IGPKWU YKVJQWV GFWECVKQP KU NKMG UKNXGT KP VJG OKPG";
     
     private static final String CAESAR_CIPHER_TEXT_3  = "JHQLXV ZLWKRXW HGXFDWLRQ LV OLNH VLOYHU LQ WKH PLQH";
     private static final String CAESAR_CIPHER_TEXT_4 = "KIRMYW AMXLSYX IHYGEXMSR MW PMOI WMPZIV MR XLI QMRI";
     private static final String CAESAR_CIPHER_TEXT_5 = "LJSNZX BNYMTZY JIZHFYNTS NX QNPJ XNQAJW NS YMJ RNSJ";
+    
     
     @Test
     public void CaesarCipher_WithPossibleKeyLengths4_returnsExpectedResult() {
@@ -38,7 +43,7 @@ public class VigenereSolverTest {
 
         List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_2);
 
-        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_2)), is(true));
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
     }
     
     @Test
@@ -49,7 +54,7 @@ public class VigenereSolverTest {
 
         List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_3);
 
-        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_2)), is(true));
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
     } 
     
     @Test
@@ -60,7 +65,7 @@ public class VigenereSolverTest {
 
         List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_4);
 
-        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_2)), is(true));
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
     }
     
     
@@ -72,6 +77,36 @@ public class VigenereSolverTest {
 
         List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_5);
 
-        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_2)), is(true));
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
+    }
+    
+    @Test
+    public void CaesarCipher_WithGoodFrequenciesDist_returnsExpectedResult() {
+    	Vigenere vigenere = new Vigenere(PLAIN_TEXT_GOOD_DIST_OF_LETTER, Language.ENGLISH);
+        String encrypText = vigenere.encrypt("G");
+        
+        VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
+                .setPossibleKeyLengths(1);
+        VigenereSolver solver = new VigenereSolver(settings);
+
+        List<SolveResult> result = solver.solve(encrypText);
+        
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_GOOD_DIST_OF_LETTER)), is(true));
+    }
+    
+    
+    @Test
+    public void CaesarCipher_WithBadFrequenciesDist_returnsExpectedResult()  {
+    	Vigenere vigenere = new Vigenere(PLAIN_TEXT_BAD_DIST_OF_LETTER, Language.ENGLISH);
+        String encrypText = vigenere.encrypt("G");
+    	
+    	VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
+                .setPossibleKeyLengths(1);
+        VigenereSolver solver = new VigenereSolver(settings);
+        
+        List<SolveResult> result = solver.solve(encrypText);
+  
+        assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_BAD_DIST_OF_LETTER)), is(false));
+
     }
 }
