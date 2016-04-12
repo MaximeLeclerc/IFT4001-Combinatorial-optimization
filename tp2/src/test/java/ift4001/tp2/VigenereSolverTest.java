2b0b94f30196b2e9e38fb3c1c94ebaa07fd087ee
Alexandre <alexandre.picardlemieux@gmail.com>
2016-04-12 14:45:38 -0400

    adding result in report, but with key length at 4 and 5, the result is false

--- tp2/src/test/java/ift4001/tp2/VigenereSolverTest.java
+++ tp2/src/test/java/ift4001/tp2/VigenereSolverTest.java
@@ -13,10 +13,10 @@ public class VigenereSolverTest {
 	private static final String PLAIN_TEXT = "FRIEN DSROM ANSCO UNTRY MENLE NDMEY OUREA RSICO METOB URYCA ESARN OTTOP RAISE HIMTH EEVIL THATM ENDOL IVESA FTERT HEMTH EGOOD ISOFT INTER REDWI THTHE IRBON ESSOL ETITB EWITH CAESA RTHEN OBLEB RUTUS HATHT OLDYO UCAES ARWAS AMBIT IOUSI FITWE RESOI TWASA GRIEV OUSFA ULTAN DGRIE VOUSL YHATH CAESA RANSW ERDIT HEREU NDERL EAVEO FBRUT USAND THERE STFOR BRUTU SISAN HONOU RABLE MANSO ARETH EYALL ALLHO NOURA BLEME NCOME ITOSP EAKIN CAESA RSFUN ERAL";
 
 	private static final String PLAIN_TEXT_SMALL = "GENIUS WITHOUT EDUCATION IS LIKE SILVER IN THE MINE";
-	private static final String PLAIN_TEXT_VERY_SMALL_TEXT = "AEIT";
+	private static final String PLAIN_TEXT_VERY_SMALL_TEXT = "AEDV";
 
 	private static final String PLAIN_TEXT_GOOD_DIST_OF_LETTER = "HEREUPON LEGRAND AROSE WITH A GRAVE AND STATELY AIR AND BROUGHT ME THE BEETLE FROM A GLASS CASE IN WHICH IT WAS ENCLOSED IT WAS A BEAUTIFUL SCARABAEUS AND AT THAT TIME UNKNOWN TO NATURALISTS OF COURSE A GREAT PRIZE IN A SCIENTIFIC POINT OF VIEW THERE WERE TWO ROUND BLACK SPOTS NEAR ONE EXTREMITY OF THE BACK AND A LONG ONE NEAR THE OTHER THE SCALES WERE EXCEEDINGLY HARD AND GLOSSY WITH ALL THE APPEARANCE OF BURNISHED GOLD THE WEIGHT OF THE INSECT WAS VERY REMARKABLE AND TAKING ALL THINGS INTO CONSIDERATION I COULD HARDLY BLAME JUPITER FOR HIS OPINION RESPECTING IT";
-	private static final String PLAIN_TEXT_BAD_DIST_OF_LETTER = "ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB ZJXQKB";
+	private static final String PLAIN_TEXT_BAD_DIST_OF_LETTER = "ZJXQKB";
 
 	private static final String CAESAR_CIPHER_TEXT_1 = "HFOJVT XJUIPVU FEVDBUJPO JT MJLF TJMWFS JO UIF NJOF";
 	private static final String CAESAR_CIPHER_TEXT_2 = "IGPKWU YKVJQWV GFWECVKQP KU NKMG UKNXGT KP VJG OKPG";
@@ -39,6 +39,17 @@ public class VigenereSolverTest {
 	}
 
 	@Test
+	public void CaesarCipher_WithPossibleKeyLengths1_returnsExpectedResult() {
+		VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
+				.setPossibleKeyLengths(1);
+		VigenereSolver solver = new VigenereSolver(settings);
+
+		List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_1);
+
+		assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
+	}
+
+	@Test
 	public void CaesarCipher_WithPossibleKeyLengths2_returnsExpectedResult() {
 		VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
 				.setPossibleKeyLengths(2);
@@ -56,17 +67,6 @@ public class VigenereSolverTest {
 		VigenereSolver solver = new VigenereSolver(settings);
 
 		List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_3);
-
-		assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
-	}
-
-	@Test
-	public void CaesarCipher_WithPossibleKeyLengths1_returnsExpectedResult() {
-		VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
-				.setPossibleKeyLengths(1);
-		VigenereSolver solver = new VigenereSolver(settings);
-
-		List<SolveResult> result = solver.solve(CAESAR_CIPHER_TEXT_1);
 
 		assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_SMALL)), is(true));
 	}
@@ -140,9 +140,21 @@ public class VigenereSolverTest {
 	public void CaesarCipher_WithVerySmallText_returnsExprectedResult() {
 		vigenere = new Vigenere(PLAIN_TEXT_VERY_SMALL_TEXT, Language.ENGLISH);
 		encrypText = vigenere.encrypt("D");
-
 		VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
 				.setPossibleKeyLengths(1);
+		VigenereSolver solver = new VigenereSolver(settings);
+
+		List<SolveResult> result = solver.solve(encrypText);
+
+		assertThat(result.stream().anyMatch(r -> r.plainText.equals(PLAIN_TEXT_VERY_SMALL_TEXT)), is(true));
+	}
+
+	@Test
+	public void CaesarCipher_WithVerySmallTextAndNumberOfResultsReturn50AndKeyLengths2_returnsExprectedResult() {
+		vigenere = new Vigenere(PLAIN_TEXT_VERY_SMALL_TEXT, Language.ENGLISH);
+		encrypText = vigenere.encrypt("DT");
+		VigenereSolver.Settings settings = new VigenereSolver.Settings().setPossibleLanguages(Language.ENGLISH)
+				.setPossibleKeyLengths(2).setNumberOfResultsToReturn(50);
 		VigenereSolver solver = new VigenereSolver(settings);
 
 		List<SolveResult> result = solver.solve(encrypText);
